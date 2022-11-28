9c2754d75b58cec55f132a102f1dc1c25eaaf5a1
CHRISTOPHE BOISSIER <101327782+Tof28@users.noreply.github.com>
2022-11-28 13:25:40 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
     $("#flechedroite").click(function () { //conditionner la  fonction au clic
 
-        var marioposition = $("#mario").position();
+        var marioposition = $("#mario").position(); // créer une variable pour récupérer la position de l'image mario
 
         var marioleft = marioposition.left;
-        marioleft += 100;
+        marioleft += 100; // incrémenter la nouvelle variable de l'image
         $("#mario").css("left", marioleft + "px");
     });
         
