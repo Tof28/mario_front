0e0cd5adb238c724064ce3392f28d8c8aa5ef6a1
CHRISTOPHE BOISSIER <101327782+Tof28@users.noreply.github.com>
2022-11-29 17:01:26 +0100

    Update site.js

--- js/site.js
+++ js/site.js
@@ -5,8 +5,8 @@ $( document ).ready(function() { //activer une fonction quand la page est charg�
 
         var marioleft = marioposition.left; // créer une autre variable à incrémenter
 
-        marioleft += 50; // incrémenter la nouvelle variable de l'image
-        if(marioleft<950) {
+        marioleft += 30; // incrémenter la nouvelle variable de l'image
+        if(marioleft<980) {
         $("#mario").css("left", marioleft + "px"); //faire avancer l'image mario modifiant sa position css à chaque clic
         }
     };
@@ -16,7 +16,7 @@ $( document ).ready(function() { //activer une fonction quand la page est charg�
 
         var marioleft = marioposition.left;
 
-        marioleft -= 50;
+        marioleft -= 30;
         if(marioleft > 0) {
         $("#mario").css("left", marioleft + "px");
         }
@@ -28,7 +28,7 @@ $( document ).ready(function() { //activer une fonction quand la page est charg�
 
         var mariotop = marioposition.top;
 
-        mariotop -= 50;
+        mariotop -= 30;
         if(mariotop>0) {
         $("#mario").css("top", mariotop + "px");
         }
@@ -39,12 +39,12 @@ $( document ).ready(function() { //activer une fonction quand la page est charg�
 
         var mariotop = marioposition.top;
 
-        mariotop += 50;
+        mariotop += 30;
         if(mariotop<500) {
         $("#mario").css("top", mariotop + "px");
         }
     }
-    $("#flechedroite").click(function () { //conditionner la  fonction au clic sur le bouton GO
+    $("#flechedroite").click(function() { //conditionner la  fonction au clic sur le bouton correspondant
         fd();
          });
     $("#flechegauche").click(function() {
@@ -61,38 +61,66 @@ $( document ).ready(function() { //activer une fonction quand la page est charg�
     $(document).keydown(function(e){
         switch (e.which){
             
-                case 37:    //left arrow key
+                case 37:    //flèche gauche
                     fg();
                     break;
-                case 38:    //up arrow key
+                case 38:    //flèche du haut
                     ft();
                     break;
-                case 39:    //right arrow key
+                case 39:    //flèche droite
                    fd();
                     break;
-                case 40:    //bottom arrow key
+                case 40:    //flècehe du bas
                    fb();
                     break;
         };
     });
 
-//Clique sur le chateau
-$(document).mousemove(function(event){
+    //Clique sur le chateau
+    $(document).mousemove(function(event){
 
-    var mouseX = event.pageX;
-    var mouseY = event.pageY;
+        var mouseX = event.pageX;
+        var mouseY = event.pageY;
 
-    var container_position = $('#main-container').position();
-    var container_positionX = container_position.left;
-    var container_positionY = container_position.top;
+        var container_position = $('#main-container').position();
+        var container_positionX = container_position.left;
+        var container_positionY = container_position.top;
 
-    $('#main').css('left', mouseX-container_positionX-50 + 'px');
-    $('#main').css('top', mouseY-container_positionY-50 + 'px');
+        $('#main').css('left', mouseX-container_positionX-50 + 'px');
+        $('#main').css('top', mouseY-container_positionY-50 + 'px');
 
-});
+    });
 
-$("#zonechateau").click(function() {
-    $('#main').css('display', 'none');
-    $('#mario').css('display', 'block');
-})
+    $("#zonechateau").click(function() {
+        $('#main').css('display', 'none');
+        $('#mario').css('display', 'block');
+    });
+
+    /*$("#victoire").click(function() {
+
+       // var message = ("Vous avez gagné!");
+        
+        $("#mario-victoire").css('display', 'block');
+        $("#message").css('display', 'block');
+
+    });*/
+
+    $("#victoire").css(function(event) {
+        var marioposition = $("#mario").position();
+
+        var chateauposition = $("#zonechateau").position();
+
+        var marioPositionX = marioposition.left;
+        var marioPositionY = marioposition.top;
+
+        var chateauPositionX = chateauposition.left;
+        var chateauPositionY = chateauposition.top;
+
+        if(marioPositionX <= chateauPositionX && marioPositionY <= chateauPositionY) {
+            $("#mario-victoire").css('display', 'block');
+            $("#message").css('display', 'block');
+        }
+
+    });
+
 });
